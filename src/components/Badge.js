5c14eea7d0821040316bcb826a342de869f55ecf
Stackbit Code Editor <projects@stackbit.com>
2021-10-14 16:06:29 +0000

    Badge.js: changed

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,25 +1,25 @@
 import classNames from "classnames";
 
 export default function Badge(props) {
-  const { label } = props;
-  const cssClasses = props.className || null;
-  const cssId = props.elementId || null;
+    const { label } = props;
+    const cssClasses = props.className || null;
+    const cssId = props.elementId || null;
 
-  if (!label) {
-    return null;
-  }
+    if (!label) {
+        return null;
+    }
 
-  return (
-    <div
-      id={cssId}
-      className={classNames(
-        "sb-component",
-        "sb-component-block",
-        "sb-component-badge",
-        cssClasses
-      )}
-    >
-      {label}
-    </div>
-  );
+    return (
+        <div
+            id={cssId}
+            className={classNames(
+                "sb-component",
+                "sb-component-block",
+                "sb-component-badge",
+                cssClasses
+            )}
+        >
+            HELLO - {label}
+        </div>
+    );
 }
