8c6a7e214ad4365d580d05c7eb3979c31d29392d
Stackbit Code Editor <projects@stackbit.com>
2021-10-14 15:55:53 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
     darkMode: false,
     theme: {
         extend: {
-            fontFamily
+            fontFamily: {}
             colors: {
                 // primary: '#207bea'
             }
